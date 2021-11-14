829b7f0f4ae5c39bdc12facebaceaef0cbe93ecb
meginuroshvili <mnuroshvili@gmail.com>
2021-11-15 00:47:12 +0400

    add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+if(document.getElementById('send')){
+    document.getElementById('send').addEventListener('click', function() {
+        alert('Thank you, Your Message Was Sent')
+    })
+}
+
+if(document.getElementById('submit')){
+    document.getElementById('submit').addEventListener('click', function() {
+        alert('Your mail was sent')
+    })
+}
+
